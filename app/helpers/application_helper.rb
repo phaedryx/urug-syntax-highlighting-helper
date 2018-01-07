4a61642dc2d74a36d67ddfdf482116ec588d5f03
Tad Thorley <phaedryx@gmail.com>
2018-01-06 19:32:19 -0700

    add more languages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,21 @@
 require 'rouge'
 
 module ApplicationHelper
-  def code(title: nil, &block)
-    source    = capture(&block)
+  LEXERS = {
+    'javascript' => Rouge::Lexers::Javascript.new,
+    'markdown' => Rouge::Lexers::Markdown.new,
+    'ruby' => Rouge::Lexers::Ruby.new,
+  }
+
+  def code(title: nil, language: nil, &block)
     formatter = Rouge::Formatters::HTMLTable.new(
       Rouge::Formatters::HTML.new,
       table_class: 'code',
       gutter_class: 'gutter',
       code_class: 'source'
     )
-    lexer     = Rouge::Lexers::Ruby.new
-    source    = formatter.format(lexer.lex(source)).html_safe
+    lexer = LEXERS[language] || LEXERS['markdown']
+    source = formatter.format(lexer.lex(capture(&block))).html_safe
 
     tag.figure(
       tag.figcaption(title, class: 'title') +
