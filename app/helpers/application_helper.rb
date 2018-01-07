120ec69d81575ad70e7344ab2735fbd4f714b65a
Tad Thorley <phaedryx@gmail.com>
2018-01-06 18:50:42 -0700

    use generic css classes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,14 @@ require 'rouge'
 module ApplicationHelper
   def code(title: nil, &block)
     source    = capture(&block)
-    formatter = Rouge::Formatters::HTMLTable.new(Rouge::Formatters::HTML.new)
+    formatter = Rouge::Formatters::HTMLTable.new(
+      Rouge::Formatters::HTML.new,
+      table_class: 'code',
+      gutter_class: 'gutter',
+      code_class: 'source'
+    )
     lexer     = Rouge::Lexers::Ruby.new
-    source    = formatter.format(lexer.lex(source))
+    source    = formatter.format(lexer.lex(source)).html_safe
 
     tag.figure(
       tag.figcaption(title, class: 'title') +
