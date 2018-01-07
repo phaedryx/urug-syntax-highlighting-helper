18a1ff548d776bbd729cdd60b244d8ca4f0512d2
Tad Thorley <phaedryx@gmail.com>
2018-01-06 18:38:29 -0700

    add rouge

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
+require 'rouge'
+
 module ApplicationHelper
   def code(title: nil, &block)
-    source = capture(&block)
+    source    = capture(&block)
+    formatter = Rouge::Formatters::HTMLTable.new(Rouge::Formatters::HTML.new)
+    lexer     = Rouge::Lexers::Ruby.new
+    source    = formatter.format(lexer.lex(source))
 
     tag.figure(
       tag.figcaption(title, class: 'title') +
